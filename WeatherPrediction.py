3efc4a63b590ddf2aa4f52216c6a9c4537d2de17
JamesShaklee <126708035+JamesShaklee@users.noreply.github.com>
2024-10-07 17:20:17 -0400

    updated my branch

--- WeatherPrediction.py
+++ WeatherPrediction.py
@@ -12,9 +12,6 @@ print(weather_data.columns)
 X = weather_data.drop(columns=['Precipitation_mm', 'Rain_Category', 'Date_Time', 'Location', 'Date', 'Time', 'Millitary_Time'])
 y = weather_data['Precipitation_mm']
 
-#IMPORTANT: The commented out code doesn't work due to the lack of class sorting
-#This commit is just to save the progress I have so far - Alex
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 print('X_train.shape = ', X_train.shape)
 print('y_train.shape = ', y_train.shape)
