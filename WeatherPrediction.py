450c5906a7c2cfc54a20424410f6e43bd89d7a53
JamesShaklee <126708035+JamesShaklee@users.noreply.github.com>
2024-10-07 17:16:23 -0400

    updated my branch

--- WeatherPrediction.py
+++ WeatherPrediction.py
@@ -1,5 +1,6 @@
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, r2_score
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,15 +9,20 @@ from sklearn.model_selection import train_test_split
 
 weather_data = pd.read_csv('weather_data_cleaned.csv')
 print(weather_data.columns)
-X = weather_data.drop(columns=['Precipitation_mm'])
+X = weather_data.drop(columns=['Precipitation_mm', 'Rain_Category', 'Date_Time', 'Location', 'Date', 'Time', 'Millitary_Time'])
 y = weather_data['Precipitation_mm']
 
 #IMPORTANT: The commented out code doesn't work due to the lack of class sorting
 #This commit is just to save the progress I have so far - Alex
-classes = np.unique(y)
-print(classes)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4524)
-dt = DecisionTreeClassifier(max_depth=3);
-#dt.fit(X_train, y_train)
-#y_train_pred = dt.predict(X_train)
-#print(accuracy_score(y_train, y_train_pred))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+print('X_train.shape = ', X_train.shape)
+print('y_train.shape = ', y_train.shape)
+print('X_test.shape = ', X_test.shape)
+print('y_test.shape = ', y_test.shape)
+dt = DecisionTreeRegressor()
+dt.fit(X_train, y_train)
+y_train_pred = dt.predict(X_train)
+print(r2_score(y_train, y_train_pred))
+y_test_pred = dt.predict(X_test)
+print(r2_score(y_test, y_test_pred))
